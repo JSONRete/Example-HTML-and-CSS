5ad23dfc277f9fc7c833e084ffba987610c68919
JSONRete <jsonrete@gmail.com>
2022-07-27 00:08:49 -0400

    added 'My Solution' to quiz

--- 5_JavaScript_and_The_DOM/Lessons/Lesson_1_Syntax/Quizes/quiz_1_3.js
+++ 5_JavaScript_and_The_DOM/Lessons/Lesson_1_Syntax/Quizes/quiz_1_3.js
@@ -1,3 +1,24 @@
+// /*
+//  * Programming Quiz: Destructuring Arrays (1-3)
+//  *
+//  * Use destructuring to initialize the variables `one`, `two`, and `three`
+//  * with the colors from the `things` array.
+//  */
+
+// const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
+
+// const one = things;
+// const two = '';
+// const three = '';
+
+// const colors = `List of Colors
+// 1. ${one}
+// 2. ${two}
+// 3. ${three}`;
+
+// console.log(colors);
+
+My Solution:
 /*
  * Programming Quiz: Destructuring Arrays (1-3)
  *
@@ -7,9 +28,10 @@
 
 const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
 
-const one = things;
-const two = '';
-const three = '';
+const [one, , , two, , , , three] = things;
+// const one = things;
+// const two = '';
+// const three = '';
 
 const colors = `List of Colors
 1. ${one}
@@ -17,3 +39,4 @@ const colors = `List of Colors
 3. ${three}`;
 
 console.log(colors);
+
