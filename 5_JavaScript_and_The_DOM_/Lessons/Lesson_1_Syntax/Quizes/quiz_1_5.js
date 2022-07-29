29ef2fc7b8bc20efda3ce67c6356e82c73ab0b00
JSONRete <jsonrete@gmail.com>
2022-07-29 03:44:51 -0400

    added My Solution

--- 5_JavaScript_and_The_DOM_/Lessons/Lesson_1_Syntax/Quizes/quiz_1_5.js
+++ 5_JavaScript_and_The_DOM_/Lessons/Lesson_1_Syntax/Quizes/quiz_1_5.js
@@ -1,11 +1,33 @@
+// /*
+//  * Programming Quiz: Using the Rest Parameter (1-5)
+//  */
+
+// // your code goes here
+
+// function average() {
+    
+// }
+
+// console.log(average(2, 6));
+// console.log(average(2, 3, 3, 5, 7, 10));
+// console.log(average(7, 1432, 12, 13, 100));
+// console.log(average());
+
+// My Solution:
 /*
  * Programming Quiz: Using the Rest Parameter (1-5)
  */
 
 // your code goes here
 
-function average() {
-    
+function average(...nums) {
+    let total = 0;  
+for(const num of nums)
+{
+    total +=num;
+    n= nums.length;
+}
+return total/n;
 }
 
 console.log(average(2, 6));
