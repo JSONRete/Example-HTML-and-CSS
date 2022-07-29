ac7c08ce3b028a0f69484c2437936765c47590ff
JSONRete <jsonrete@gmail.com>
2022-07-29 03:09:38 -0400

    added 'My Solution'

--- 5_JavaScript_and_The_DOM_/Lessons/Lesson_1_Syntax/spread_operator.js
+++ 5_JavaScript_and_The_DOM_/Lessons/Lesson_1_Syntax/spread_operator.js
@@ -8,3 +8,17 @@
 // const produce = [];
 
 // console.log(produce);
+
+
+// My Solution:
+
+/*
+ * Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+ */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
